238dbc25fc40949d604ac95129b37a93c87f4e49
Donovan Hutchence <slimbuck7@gmail.com>
2025-07-10 13:42:43 +0100

    latest

---
+++ src/gpu/gpu-device.ts
@@ -0,0 +1,100 @@
+import { Worker } from 'node:worker_threads';
+import { create, globals } from 'webgpu';
+import {
+    PIXELFORMAT_BGRA8,
+    Application,
+    Texture,
+    WebgpuGraphicsDevice
+} from 'playcanvas';
+
+import { Column, DataTable } from '../data-table.js';
+
+import { JSDOM } from 'jsdom';
+
+Object.assign(globalThis, globals);
+
+const jsdomSetup = () => {
+    const html = '<!DOCTYPE html><html><head></head><body></body></html>';
+
+    const jsdom = new JSDOM(html, {
+        resources: 'usable',         // Allow the engine to load assets
+        runScripts: 'dangerously',   // Allow the engine to run scripts
+        url: 'http://localhost:3000' // Set the URL of the document
+    });
+
+    // Copy the window and document to global scope
+    // @ts-ignore
+    global.window = jsdom.window;
+    global.document = jsdom.window.document;
+
+    // Copy the DOM APIs used by the engine to global scope
+    global.ArrayBuffer = jsdom.window.ArrayBuffer;
+    global.Audio = jsdom.window.Audio;
+    global.DataView = jsdom.window.DataView;
+    global.Image = jsdom.window.Image;
+    global.KeyboardEvent = jsdom.window.KeyboardEvent;
+    global.MouseEvent = jsdom.window.MouseEvent;
+    global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
+};
+
+class GpuDevice {
+    app: Application;
+
+    backbuffer: Texture;
+
+    constructor(app: Application, backbuffer: Texture) {
+        this.app = app;
+        this.backbuffer = backbuffer;
+    }
+
+    kmeans(dataTable: DataTable, k: number, iterations = 10) {
+        const centroids = new DataTable(dataTable.columns.map(c => new Column(c.name, new Float32Array(k))));
+        const labels = new Uint32Array(k);
+
+        return { centroids, labels };
+    }
+};
+
+const createDevice = async () => {
+    jsdomSetup();
+
+    // @ts-ignore
+    globalThis.Worker = Worker;
+
+    // @ts-ignore
+    window.navigator.gpu = create([]);
+
+    const canvas = document.createElement('canvas');
+
+    canvas.width = 1024;
+    canvas.height = 512;
+
+    const graphicsDevice = new WebgpuGraphicsDevice(canvas, {
+        antialias: false,
+        depth: true,
+        stencil: false
+    });
+
+    await graphicsDevice.createDevice();
+
+    // create the application
+    const app = new Application(canvas, { graphicsDevice });
+
+    // create external backbuffer
+    const backbuffer = new Texture(graphicsDevice, {
+        width: 1024,
+        height: 512,
+        name: 'WebgpuInternalBackbuffer',
+        mipmaps: false,
+        format: PIXELFORMAT_BGRA8
+    });
+
+    // @ts-ignore
+    graphicsDevice.externalBackbuffer = backbuffer;
+
+    app.start();
+
+    return new GpuDevice(app, backbuffer);
+};
+
+export { createDevice, GpuDevice };
