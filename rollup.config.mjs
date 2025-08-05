6e5c6e14d8d05531bb75b877c8a54d54b1814ae5
praveenpenumaka <praveen9920@gmail.com>
2025-08-05 16:35:19 +0530

    adds support for viewer app generation from ply

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
+import { string } from 'rollup-plugin-string';
+import sass from 'sass';
+import scss from 'rollup-plugin-scss';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+const PCUI_DIR = path.resolve('node_modules/@playcanvas/pcui');
+
 
 const application = {
     input: 'src/index.ts',
@@ -15,6 +23,21 @@ const application = {
         typescript(),
         resolve(),
         json(),
+        string({
+            include: ['submodules/supersplat-viewer/dist/*']
+        }),
+        scss({
+            sourceMap: true,
+            runtime: sass,
+            processor: (css) => {
+                return postcss([autoprefixer])
+                    .process(css, { from: undefined })
+                    .then(result => result.css);
+            },
+            fileName: 'index.css',
+            includePaths: [`${PCUI_DIR}/dist`],
+            exclude: ['submodules/**']
+        }),
     ],
     cache: false
 };
