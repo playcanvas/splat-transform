c8f143bef65daf1bab3d2a7db83efdc825cbc069
Will Eastcott <will@playcanvas.com>
2025-10-11 17:15:56 +0100

    Support specifying settings.json to configure the HTML Viewer (#82)
    * Support specifying settings.json to configure the HTML Viewer
    
    * Help tweaks
    
    * Reformat call to writeHtml
    
    * Remove newline
    
    * Trim CLI help

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,7 @@ type Options = {
     version: boolean,
     cpu: boolean,
     iterations: number,
-    cameraPos: Vec3,
-    cameraTarget: Vec3
+    viewerSettingsPath?: string
 };
 
 const fileExists = async (filename: string) => {
@@ -136,7 +135,7 @@ const writeFile = async (filename: string, dataTable: DataTable, options: Option
                 });
                 break;
             case 'html':
-                await writeHtml(outputFile, dataTable, options.cameraPos, options.cameraTarget, options.iterations, options.cpu ? 'cpu' : 'gpu');
+                await writeHtml(outputFile, dataTable, options.iterations, options.cpu ? 'cpu' : 'gpu', options.viewerSettingsPath);
                 break;
         }
 
@@ -237,8 +236,7 @@ const parseArguments = () => {
             version: { type: 'boolean', short: 'v' },
             cpu: { type: 'boolean', short: 'c' },
             iterations: { type: 'string', short: 'i' },
-            'camera-pos': { type: 'string', short: 'C' },
-            'camera-target': { type: 'string', short: 'T' },
+            'viewer-settings': { type: 'string', short: 'E' },
 
             // file options
             translate: { type: 'string', short: 't', multiple: true },
@@ -291,14 +289,14 @@ const parseArguments = () => {
     };
 
     const files: File[] = [];
+
     const options: Options = {
         overwrite: v.overwrite ?? false,
         help: v.help ?? false,
         version: v.version ?? false,
         cpu: v.cpu ?? false,
         iterations: parseInteger(v.iterations ?? '10'),
-        cameraPos: parseVec3((v as any)['camera-pos'] ?? '2,2,-2'),
-        cameraTarget: parseVec3((v as any)['camera-target'] ?? '0,0,0')
+        viewerSettingsPath: (v as any)['viewer-settings']
     };
 
     for (const t of tokens) {
@@ -447,18 +445,17 @@ GLOBAL OPTIONS
     -w, --overwrite                            Overwrite output file if it exists.
     -c, --cpu                                  Use CPU for spherical harmonic compression.
     -i, --iterations       <n>                 Iterations for SOG SH compression (more = better). Default: 10.
-    -C, --camera-pos       <x,y,z>             HTML viewer camera position. Default: (2, 2, -2).
-    -T, --camera-target    <x,y,z>             HTML viewer target position. Default: (0, 0, 0).
+    -E, --viewer-settings  <settings.json>     HTML viewer settings JSON file.
 
 EXAMPLES
     # Scale then translate
-    splat-transform bunny.ply -s 0.5 -t 0,0,10 bunny_scaled.ply
+    splat-transform bunny.ply -s 0.5 -t 0,0,10 bunny-scaled.ply
 
     # Merge two files with transforms
     splat-transform -w cloudA.ply -r 0,90,0 cloudB.ply -s 2 merged.compressed.ply
 
-    # HTML viewer with custom camera
-    splat-transform -C 0,0,0 -T 0,0,10 bunny.ply bunny_app.html
+    # Generate HTML viewer with custom settings
+    splat-transform -E settings.json bunny.ply bunny-viewer.html
 
 GENERATORS (beta)
     # Generate synthetic splats
